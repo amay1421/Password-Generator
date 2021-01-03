4be1c1abf8b3851bb01a1100951ed7edd4c0c0f5
Alyson May <amay1421@yahoo.com>
2021-01-03 17:40:38 -0600

    Letter set update

--- script.js
+++ script.js
@@ -30,9 +30,11 @@ function generatePassword() {
 
   // create character pool/sets
 
-  var lowerCaseSet = ['a','e', 'i', 'o', 'u'];
+  var lowerCaseSet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+  't','u','v','w','x','y','z',];
 
-  var upperCaseSet = ['A', 'E', 'I', 'O', 'U'];
+  var upperCaseSet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
+  'T','U','V','W','X','Y','Z',];
 
   var finalSet = [];
 
